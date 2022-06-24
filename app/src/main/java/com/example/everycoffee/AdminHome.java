e0b52bbeda8df5ed0141d72e3b2999d5101e088d
Nur Sakinah <nursakinah2402@gmail.com>
2022-06-24 17:12:26 +0700

    Update Admin Menu and Home

--- app/src/main/java/com/example/everycoffee/AdminHome.java
+++ app/src/main/java/com/example/everycoffee/AdminHome.java
@@ -2,13 +2,49 @@ package com.example.everycoffee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class AdminHome extends AppCompatActivity {
+    private ImageView ivMenu, ivTrx;
+    private Button logoutAdm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
+
+        ivMenu =  findViewById(R.id.img_menu);
+        ivTrx =  findViewById(R.id.img_transaction);
+        logoutAdm =  findViewById(R.id.admin_logout_button);
+
+        ivMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminHome.this, AdminMenu.class);
+                startActivity(intent);
+            }
+        });
+
+        ivTrx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminHome.this, AdminMenu.class);
+                startActivity(intent);
+            }
+        });
+
+        logoutAdm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminHome.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/everycoffee/AdminMenu.java
+++ app/src/main/java/com/example/everycoffee/AdminMenu.java
@@ -1,14 +1,86 @@
 package com.example.everycoffee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.everycoffee.ViewHolder.ProductViewHolder;
+import com.example.everycoffee.model.Product;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class AdminMenu extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    private Button fab;
+    private DatabaseReference ProductRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_menu);
+
+        ProductRef = FirebaseDatabase.getInstance().getReference().child("Product");
+        recyclerView = findViewById(R.id.recycler_menu);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        fab = findViewById(R.id.add_button);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminMenu.this, AdminAddProduct.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Product> options =
+                new FirebaseRecyclerOptions.Builder<Product>()
+                        .setQuery(ProductRef, Product.class)
+                        .build();
+        FirebaseRecyclerAdapter<Product, ProductViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Product, ProductViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull final Product model) {
+                        holder.txtProductName.setText(model.getProductName());
+                        holder.txtproductPrice.setText("Price : Rp. " + model.getPrice());
+                        Picasso.get().load(model.getImage()).into(holder.imageView);
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(AdminMenu.this, DetailProduct.class);
+                                intent.putExtra("pid", model.getPid());
+                                startActivity(intent);
+                            }
+                        });
+                    }
+
+                    @NonNull
+                    @Override
+                    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewtype) {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_coffee_item_layout, parent,false);
+                        ProductViewHolder holder = new ProductViewHolder(view);
+                        return holder;
+                    }
+                };
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
     }
 }
