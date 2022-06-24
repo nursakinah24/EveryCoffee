50e1a3089b20e7335b83b740b90694a3fa4ff643
Annisa Divayu Andriyani <95303337+eunannana@users.noreply.github.com>
2022-06-24 11:25:53 +0700

    update add menu layout

--- app/src/main/java/com/example/everycoffee/AdminAddProduct.java
+++ app/src/main/java/com/example/everycoffee/AdminAddProduct.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class admin_menu extends AppCompatActivity {
+public class AdminAddProduct extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_menu);
+        setContentView(R.layout.activity_admin_add_product);
     }
 }
