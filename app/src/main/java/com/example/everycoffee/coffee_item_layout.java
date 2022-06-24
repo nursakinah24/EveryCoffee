a493cba9a9122c62d9f2c104a9e05bb8ba94f6b7
Annisa Divayu Andriyani <95303337+eunannana@users.noreply.github.com>
2022-06-24 10:54:58 +0700

    update item layout

---
+++ app/src/main/java/com/example/everycoffee/coffee_item_layout.java
@@ -0,0 +1,14 @@
+package com.example.everycoffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class coffee_item_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coffee_item_layout);
+    }
+}
