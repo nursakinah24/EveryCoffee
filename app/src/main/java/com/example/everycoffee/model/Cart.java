21dfb2c8afc35b334c0e37c9a224b1cd3f32be8e
Nur Sakinah <nursakinah2402@gmail.com>
2022-06-25 11:35:35 +0700

    Adding Class for Cart and CartViewHolder

---
+++ app/src/main/java/com/example/everycoffee/model/Cart.java
@@ -0,0 +1,48 @@
+package com.example.everycoffee.model;
+
+public class Cart {
+    private String pid, productName, price, quantity;
+
+    public Cart() {
+    }
+
+    public Cart(String pid, String productName, String price, String quantity) {
+        this.pid = pid;
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+}
