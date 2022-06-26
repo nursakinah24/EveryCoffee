fbbd310791afd2e23673325a8ae795b4ab263bb3
Annisa Divayu Andriyani <95303337+eunannana@users.noreply.github.com>
2022-06-26 13:43:24 +0700

    fix user home layout

---
+++ app/src/main/java/com/example/everycoffee/UserOrderHistory.java
@@ -0,0 +1,14 @@
+package com.example.everycoffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserOrderHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_order_history);
+    }
+}
